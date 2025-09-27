e53c53a15a3691827ffd4357e3ccbf13aa7e3e60
25HP4283 25HP4283 <25HP4283@G301-CC0211.local>
2025-09-27 12:01:13 +0800

    hi

--- controller/post/post.find.js
+++ controller/post/post.find.js
@@ -1,11 +1,7 @@
 import { postModel } from "../../post.schema.js";
 
 export const findPost = async (req, res) => {
-  const id = req.params.id;
-  console.log(id);
-  const posts = await postModel.find({
-    userId: id,
-  });
+  const posts = await postModel.find();
   console.log(posts);
   res.json(posts);
 };
--- router/post.route.js
+++ router/post.route.js
@@ -3,5 +3,5 @@ import { createPost } from "../controller/post/post.create.js";
 import { findPost } from "../controller/post/post.find.js";
 const postRouter = express.Router();
 postRouter.post("/create", createPost);
-postRouter.post("/posts/:id", findPost);
+postRouter.post("/posts", findPost);
 export default postRouter;
